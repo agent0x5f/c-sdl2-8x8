2519fb3b03f9d55bad7a91871b74bd7b52466306
Mat. Hdez <mmateo95@protonmail.com>
2021-03-08 18:27:01 -0600

    Add files via upload

---
+++ cproyect/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <SDL2/SDL.h>
+
+int main(int argv, char** args)
+{
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Window *window = SDL_CreateWindow("Hello SDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, 0);
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+
+	bool isRunning = true;
+	SDL_Event event;
+	int timer=100;
+	int color=255;
+	while (timer>0)
+	{
+		while (SDL_PollEvent(&event))
+		{
+			switch (event.type)
+			{
+			case SDL_QUIT:
+				isRunning = false;
+				break;
+
+			case SDL_KEYDOWN:
+				color=0;
+			}
+		}
+	timer--;
+		SDL_RenderClear(renderer);
+		SDL_SetRenderDrawColor(renderer, color, 0, 0, 255);
+
+		SDL_RenderPresent(renderer);
+	}
+
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
+	return 0;
+}
